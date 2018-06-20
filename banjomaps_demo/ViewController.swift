a9b846b7769f3b1ad6eb7182ff91f23cc8c044a0
Dmitrii Zverev <dima.newmail@gmail.com>
2018-06-20 20:40:41 +1000

    Initial Commit

---
+++ banjomaps_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  banjomaps_demo
+//
+//  Created by Dmitrii Zverev on 20/6/18.
+//  Copyright © 2018 Dmitrii Zverev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
